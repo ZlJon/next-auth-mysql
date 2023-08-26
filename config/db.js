b1a1182ab2e9ffbea584aff3d23fc6123fe27cbd
sub-bong <112574397+ZlJon@users.noreply.github.com>
2023-08-27 04:44:32 +0900

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ import mysql from "serverless-mysql";
 
 export const pool = mysql({
   config: {
-    host: "localhost",
-    user: "root",
-    password: "min1547",
-    port: "3306",
-    database: "next_auth_test",
+    host: "",
+    user: "",
+    password: "",
+    port: "",
+    database: "",
   },
 });
